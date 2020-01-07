f225effb7869cf0231e535bf9bb44e5dfcef167f
Kishore <Krishnasetlur210@gmail.com>
2020-01-07 23:28:53 +0530

    Added transactions list

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+final List<Transaction> transactions = [
+  Transaction[id:'t1', title: 'New Shoes', amount:69.99, date: DateTime.now()],
+  Transaction[id:'t2', title: 'Weekly Groceries', amount:16.53, date: DateTime.now()],
+
+];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/foundation.dart';
+
 class Transactiont {
-  
+  final String id;
+  final String title;
+  final double amount;
+   final DateTime date;
+
+  Transaction({
+    @required this.id, @required this.title, @required this.amount, @required this.date})
 }
