3bab0ba4ef150e960ede1fca4d1eef300657b67a
Kishore S R <Kishore.sr@mobinius.com>
2020-01-22 18:23:47 +0530

    ListView added

--- lib/main.dart
+++ lib/main.dart
@@ -28,22 +28,24 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
-        body: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Container(
-                  color: Colors.blue,
-                  child: Text('CHART'),
+        body: SingleChildScrollView(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Container(
+                    color: Colors.blue,
+                    child: Text('CHART'),
+                  ),
+                  elevation: 5,
                 ),
-                elevation: 5,
               ),
-            ),
-            UserTransactions()
-          ],
+              UserTransactions()
+            ],
+          ),
         ));
   }
 }
