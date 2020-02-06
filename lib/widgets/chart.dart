d4ab3df62bd8388cfd379039053323d6d5b94748
Kishore <Krishnasetlur210@gmail.com>
2020-02-07 00:00:04 +0530

    Displayed chart for the transactions

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,9 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+import '../widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTrasactions;
+
+  Chart(this.recentTrasactions);
+
+  List<Map<String, Object>> get groupTrasactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTrasactions.length; i++) {
+        if (recentTrasactions[i].date.day == weekDay.day &&
+            recentTrasactions[i].date.month == weekDay.month &&
+            recentTrasactions[i].date.year == weekDay.year) {
+          totalSum += recentTrasactions[i].amount;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum,
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupTrasactionValues.fold(0.0, (sum, item) {
+      return sum += item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(elevation: 6, margin: EdgeInsets.all(20),
-    child: Row(),);
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupTrasactionValues.map((data) {
+          return ChartBar(
+            data['day'],
+            data['amount'],
+            totalSpending == 0.0
+                ? 0.0
+                : (data['amount'] as double) / totalSpending,
+          );
+        }).toList(),
+      ),
+    );
   }
 }
